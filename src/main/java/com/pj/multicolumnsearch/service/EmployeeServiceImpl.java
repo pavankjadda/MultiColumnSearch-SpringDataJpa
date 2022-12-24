ca1296ac20a8396a398ae1649a59cede6cce4467
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-12-23 21:43:14 -0500

    feat(core): adds spring-boot support

--- src/main/java/com/pj/multicolumnsearch/service/EmployeeServiceImpl.java
+++ src/main/java/com/pj/multicolumnsearch/service/EmployeeServiceImpl.java
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Predicate;
 
 /**
  * @author Pavan Jadda
